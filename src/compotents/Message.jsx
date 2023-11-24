af92762ebdfa77dd66aeb8eb87577548f9bc5961
OmarKing015 <66084757+OmarKing015@users.noreply.github.com>
2023-11-25 01:17:04 +0200

    Optimizing the message on mobile devices

--- src/compotents/Message.jsx
+++ src/compotents/Message.jsx
@@ -1,22 +1,53 @@
 /* eslint-disable no-unused-vars */
 
-import React, {useState, useContext, useEffect, useRef } from "react";
+import React, { useState, useContext, useEffect, useRef } from "react";
 import pic from "./pic.jpg";
 import pic2 from "../assets/Omar.jpg";
 import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
 import { getStorage, ref, getDownloadURL } from "firebase/storage";
 const Message = ({ message }) => {
- //How to download files from firebase?
+  const TextWithBreaks = ({ text, maxCharsPerLine }) => {
+    const lines = [];
+    let currentLine = "";
 
+    // Split the text into lines
+    for (let i = 0; i < text.length; i++) {
+      currentLine += text[i];
+
+      if (currentLine.length === maxCharsPerLine) {
+        lines.push(currentLine);
+        currentLine = "";
+      }
+    }
+
+    // Add the remaining text as the last line
+    if (currentLine.length > 0) {
+      lines.push(currentLine);
+    }
+
+    // Render each line with a <br> tag
+    return (
+      <div>
+        {lines.map((line, index) => (
+          <React.Fragment key={index}>
+            {line}
+            {index < lines.length - 1 && <br />}
+          </React.Fragment>
+        ))}
+      </div>
+    );
+  };
   const { currentUser } = useContext(AuthContext);
   const { data } = useContext(ChatContext);
-const Ref= useRef();
-useEffect(()=>{
-  Ref.current?.scrollIntoView({behavior:"smooth"})
-},[message])
+  const Ref = useRef();
+  useEffect(() => {
+    Ref.current?.scrollIntoView({ behavior: "smooth" });
+  }, [message]);
+
   return (
-    <div ref={ref}
+    <div
+      ref={ref}
       className={`message ${message.senderId === currentUser.uid && "owner"}`}
     >
       <div className="messageInfo">
@@ -31,11 +62,16 @@ useEffect(()=>{
         {/* {message.date && <span>{message.date}</span>} */}
       </div>
       <div className="messageContent">
-        <p>{message.text}</p>
-        {message.img &&<><img src={message.img} alt="" /></> }
-  
-       
-        
+        <p>
+          {" "}
+          <TextWithBreaks text={message.text} maxCharsPerLine={20} />
+        </p>
+
+        {message.img && (
+          <>
+            <img src={message.img} alt="" />
+          </>
+        )}
       </div>
     </div>
   );
